ff4f7e186bfe0cd72a15be66d2a297f950de33ae
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-01-23 21:20:51 +0200

    Literals and Escape Sequences

---
+++ Project_1/Project_1/Project_1.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+     cout << "\a \n";
+     cout << "Dear Sir\\Madam," << endl << endl;
+     cout << " How are You?" << endl << endl;
+     cout << " My name is \"Alsaeed\", Nice to meet you." << endl << endl;
+
+     cout << "Mohamed\t Ali\t Omer\n";
+     cout << "Magdi\t Sabir\t Ahmed\n";
+
+
+     return 0;
+
+}
