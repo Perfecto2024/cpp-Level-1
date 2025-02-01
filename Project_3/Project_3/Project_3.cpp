8cd6d27251acdab8fcf854a8c10d28005594837e
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-02-01 21:07:20 +0200

    ok

---
+++ Project_3/Project_3/Project_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x = 20;
+    int y = 30;
+    int z = 10;
+
+
+    cout << "Num1\t" << x << " + " << endl;
+    cout << "Num2\t" << y << " + " << endl;
+    cout << "Num3\t" << z << endl;
+    cout << "------------------------" << endl;
+    cout << "Total = " << x + y + z << endl;
+
+    return 0;
+}
