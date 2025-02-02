a92a897cebff73df8d0caacdc7b1ba364eae0f97
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-02-02 23:58:47 +0200

    input data

---
+++ Project_4/Project_4/Project_4.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string Name;
+    int Age;
+    string  City;
+    string Country;
+    int MonthlySalary;
+    char Gender;
+    bool Married;
+
+    cout << "Please enter your informations!" << endl << endl;
+
+    cout << "Enter your name !" << endl;
+    cin >> Name;
+
+    cout << "Enter your age !" << endl;
+    cin >> Age;
+
+    cout << "Enter your city !" << endl;
+    cin >> City;
+
+    cout << "Enter your Country !" << endl;
+    cin >> Country;
+
+    cout << "Enter your MonthlySalary !" << endl;
+    cin >> MonthlySalary;
+
+    cout << "Enter your Gender !" << endl;
+    cin >> Gender;
+
+    cout << "Enter your Married !" << endl;
+    cin >> Married;
+
+    cout << "****************************** " << endl;
+    cout << "  Name: " << Name << endl;
+    cout << "  Age : " << Age << "Years old." << endl;
+    cout << "  City : " << City << endl;
+    cout << "  Country : " << Country << endl;
+    cout << "  Monthly Salary : " << MonthlySalary << "$" << endl;
+    cout << "  Yearly Salary : " << MonthlySalary * 12 << "$" << endl;
+    cout << "  Gender : " << Gender << endl;
+    cout << "  Married : " << Married << endl;
+    cout << "******************************\a " << endl;
+
+
+    return 0;
+}
