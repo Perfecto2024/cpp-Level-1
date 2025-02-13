06f20ced32d47728a70b491e2337a8f7bddb04d7
DELL@Alsaeed <alsaeedalsafer@gmail.com>
2025-02-13 20:09:48 +0200

    Arithemtic Operators

---
+++ Project_5/Project_5/Project_5.cpp
@@ -0,0 +1,24 @@
+
+#include <iostream>
+using namespace std;
+
+int A;
+int B;
+
+int main()
+{
+    cout << "Please enter the first number A " << endl;
+    cin >> A;
+
+    cout << "Please enter the first number B " << endl;
+    cin >> B;
+
+    cout << " A + B = " << A + B << endl;
+    cout << " A - B = " << A - B << endl;
+    cout << " A * B = " << A * B << endl;
+    cout << " A / B = " << A / B << endl;
+    cout << " A % B = " << A % B << endl;
+
+    return 0;
+
+}
